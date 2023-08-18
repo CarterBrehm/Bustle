362ccf6131d2a4fac095d6905672d9bf331081c6
Carter Brehm <carter@brehmit.com>
2023-08-18 14:50:46 -0500

    Initial Commit

---
+++ Bustle/BustleApp.swift
@@ -0,0 +1,17 @@
+//
+//  BustleApp.swift
+//  Bustle
+//
+//  Created by Carter Brehm on 8/18/23.
+//
+
+import SwiftUI
+
+@main
+struct BustleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bustle/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Bustle
+//
+//  Created by Carter Brehm on 8/18/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
