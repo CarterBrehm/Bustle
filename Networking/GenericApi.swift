98efc3196bef4dd3cd6fb5538e3be9d9c28f657c
Carter Brehm <carter@brehmit.com>
2024-01-25 11:08:16 -0600

    only check for HTTP error when we aren't mocking

--- Networking/GenericApi.swift
+++ Networking/GenericApi.swift
@@ -9,6 +9,18 @@ protocol GenericAPI {
 extension GenericAPI {
     func fetch<T: Codable>(type: T.Type, with request: URLRequest) async throws -> T {
         let (data, response) = try await session.data(for: request)
+        if let url = request.url?.absoluteString {
+            if (url.contains("file")) {
+                // we're in mock mode... don't check for HTTP errors
+            } else {
+                guard let httpResponse = response as? HTTPURLResponse else {
+                    throw APIError.requestFailed(description: "Invalid response")
+                }
+                guard httpResponse.statusCode == 200 else {
+                    throw APIError.responseUnsuccessful(description: "Status code: \(httpResponse.statusCode)")
+                }
+            }
+        }
         do {
             let decoder = JSONDecoder()
             return try decoder.decode(type, from: data)
